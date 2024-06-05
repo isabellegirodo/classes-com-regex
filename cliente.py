a11427513c19b55c0cc04c05fde1b7f845a6b22b
isabellegirodo <isabelle.girodo@uol.com.br>
2024-06-05 15:54:36 -0300

    Classe Cliente criada

---
+++ cliente.py
@@ -0,0 +1,15 @@
+import re
+
+class Cliente:
+    def mostrar_cpf(self):
+        cpf = str(input('Digite seu cpf: '))
+        padrao = r'\d{11}'
+        verifica_cpf = re.fullmatch(padrao, cpf)
+        
+        if verifica_cpf:
+            return f'Cpf formatado: {cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}'
+        else:
+            raise ValueError('Cpf inválido. O cpf deve conter 11 dígitos.')
+
+c = Cliente()
+print(c.mostrar_cpf())
